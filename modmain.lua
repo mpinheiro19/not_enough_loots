0f054555a4a6a742a2b31bd50d0db1d575f88c46
Marco Pinheiro <mpinheiro19@gmail.com>
2024-07-28 01:45:49 -0300

    bug: unable to dynamic get number of players using SetLootSetupFn

--- modmain.lua
+++ modmain.lua
@@ -1,8 +1,17 @@
 local modded  = GetModConfigData("ExtraDrop")
 local player_tag = {"player"}
 
+-- TODO: Entender como usar essa funcao para calcular corretamenta o numero de players
 local function SetLootSetupFn(lootdropper)
   local inst = lootdropper.inst
+end
+
+local function GetNumberOfNearbyPLayers(inst)
+  local x,y,z = inst.Transform:GetWorldPosition()
+  -- shoutout to DiogoW for helping me to figure this out
+  local num_player = GLOBAL.TheSim:CountEntities(x,y,z, 50, player_tag)
+
+  return num_player
 end
 
 local function AddExtraLoot(inst)
@@ -16,9 +25,7 @@ local function AddMobCustomLoot(inst)
 
   if GLOBAL.TheWorld.ismastersim then
 
-    local x,y,z = inst.Transform:GetWorldPosition()
-    -- shoutout to DiogoW for helping me to figure this out
-    local num_player = GLOBAL.TheSim:CountEntities(x,y,z, 30, player_tag)
+    local num_player = GetNumberOfNearbyPLayers(inst)
 
     if num_player > 1 then
       -- if num_player = 1 then normal loot in applied
